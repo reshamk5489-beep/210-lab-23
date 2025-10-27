432c743c1422bc3df462ffebc7af52face10f369
Resham <resha@MacBook-Air-2.local>
2025-10-26 17:15:58 -0700

    Initial Commit - Task 1

--- Goat.h
+++ Goat.h
@@ -12,8 +12,25 @@ private:
     string color;
 public: 
     Goat()                          { name = ""; age = 0; color = ""; }
-    // write three more constructors
 
+    Goat(string n)
+    {
+        name = n;
+    }
+
+    Goat(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    Goat(string n, int a, string c)
+    {
+        name = n;
+        age = a;
+        color = c;
+    }
+    
     // setters and getters
     void set_name(string n)         { name = n; };
     string get_name() const         { return name; };
